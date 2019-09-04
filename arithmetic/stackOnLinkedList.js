511dca8d8c1da264acf441e407c8fd0dbf18bda3
Corse <hmh9925991@gmail.com>
2019-09-04 21:25:34 +0800

    模拟浏览器前进后退

--- arithmetic/stackOnLinkedList.js
+++ arithmetic/stackOnLinkedList.js
@@ -26,6 +26,18 @@ class Stack{
     this.top = this.top.next;
     return node;
   }
+
+  clear() {
+    this.top = null;
+  }
+
+  display(){
+    let currentStack = this.top;
+    while(currentStack) {
+      console.log(currentStack.val);
+      currentStack = currentStack.next;
+    }
+  }
 }
 
 const stack = new Stack();
@@ -39,3 +51,59 @@ console.log(stack.pop())
 console.log(stack.pop())
 console.log(stack.pop())
 console.log(stack.pop());
+
+class SampleBrowser{
+  constructor() {
+    this.normalStack = new Stack();
+    this.backStack = new Stack();
+  }
+
+  normal(name) {
+    this.normalStack.push(name);
+    this.backStack.clear();
+    this.displayAllStack();
+  }
+
+  prev(){
+    let val = this.normalStack.pop();
+    if(val !== -1) {
+      this.backStack.push(val);
+      this.displayAllStack();
+    }else {
+      console.log('无法后退')
+    }
+  }
+
+  next() {
+    let val = this.backStack.pop();
+    if(val !== -1) {
+      this.normalStack.push(val);
+      this.displayAllStack();
+    }else {
+      console.log('无法前进');
+    }
+  }
+
+  displayAllStack() {
+    console.log('可后退页面---------');
+    this.normalStack.display();
+    console.log('可前进页面---------');
+    this.backStack.display();
+    console.log('结束----------------')
+  }
+
+}
+
+const browser = new SampleBrowser();
+
+browser.normal('www.google.com');
+browser.normal('www.sina.com');
+browser.normal('www.baidu.com');
+browser.normal('www.qq.com');
+
+browser.prev();
+browser.prev();
+
+browser.next();
+
+browser.normal('www.douyu.com')
