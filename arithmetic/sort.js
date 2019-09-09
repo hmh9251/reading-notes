431c84fc583dc5ccd7578f421bce3c079a9c110d
Corse <hmh9925991@gmail.com>
2019-09-09 22:33:11 +0800

    冒泡排序和插入排序

---
+++ arithmetic/sort.js
@@ -0,0 +1,41 @@
+
+//冒泡排序
+const bubbleSort = arr => {
+  if(arr.length <= 1) return arr;
+  let length = arr.length;
+  for(let i = 0; i < length; i++) {
+    let haschange = false;
+    for(let j = 0; j < length-i-1; j++) {
+      if(arr[j] > arr[j + 1]) {
+        let temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+        haschange = true;
+      }
+    }
+    if(!haschange) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2,5,1,6,2,7,9]))
+
+//插入排序
+const insertionSort = arr => {
+  if(arr.length <= 1) return arr;
+  for(let i = 1; i < arr.length; ++i) {
+    let value = arr[i];
+    let j = i - 1;
+    for(; j >= 0; --j) {
+      if(arr[j] > value) {
+        arr[j+1] = arr[j];
+      } else {
+        break;
+      }
+    }
+    arr[j+1] = value;
+  }
+  return arr;
+}
+
+console.log(insertionSort([5,6,2,5,2,53,9,5,0,2,5,3,2,6,1]));
