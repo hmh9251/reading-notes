1408f7a345053e3ac74285e8b83c53edd3f32be3
Corse <hmh9925991@gmail.com>
2019-09-11 21:56:27 +0800

    归并排序

--- arithmetic/sort.js
+++ arithmetic/sort.js
@@ -58,3 +58,39 @@ const selectionSort = arr => {
 }
 
 console.log(insertionSort([5,6,2,5,2,53,9,5,0,2,5,3,2,6,1]));
+
+//归并排序
+const mergeArr = (left, right) => {
+  let temp = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(left.length > leftIndex && right.length > rightIndex) {
+    if(left[leftIndex] <= right[rightIndex]) {
+      temp.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      temp.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return temp.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const mergeSort = (arr) => {
+  if(arr.length <= 1) return arr;
+  const middle = Math.floor(arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+  return mergeArr(mergeSort(left), mergeSort(right));
+}
+
+const testArr = []
+let i = 0
+while (i < 100) {
+    testArr.push(Math.floor(Math.random() * 1000))
+    i++
+}
+
+const res = mergeSort(testArr)
+console.log(testArr)
+console.log(res)
