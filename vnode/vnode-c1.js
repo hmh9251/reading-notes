8edaef3a65da53f3f44527970e9ccb078481b93f
minghang.hu <hmh9925991@gmail.com>
2020-06-12 17:34:33 +0800

    一些动画原型

---
+++ vnode/vnode-c1.js
@@ -0,0 +1,563 @@
+const VNodeFlags = {
+  // html 标签
+  ELEMENT_HTML: 1,
+  // SVG 标签
+  ELEMENT_SVG: 1 << 1,
+
+  // 普通有状态组件
+  COMPONENT_STATEFUL_NORMAL: 1 << 2,
+  // 需要被keepAlive的有状态组件
+  COMPONENT_STATEFUL_SHOULD_KEEP_ALIVE: 1 << 3,
+  // 已经被keepAlive的有状态组件
+  COMPONENT_STATEFUL_KEPT_ALIVE: 1 << 4,
+  // 函数式组件
+  COMPONENT_FUNCTIONAL: 1 << 5,
+
+  // 纯文本
+  TEXT: 1 << 6,
+  // Fragment
+  FRAGMENT: 1 << 7,
+  // Portaly
+  PORTAL: 1 << 8,
+};
+
+// html 和 svg 都是标签元素，可以用 ELEMENT 表示
+VNodeFlags.ELEMENT = VNodeFlags.ELEMENT_HTML | VNodeFlags.ELEMENT_SVG;
+// 普通有状态组件、需要被keepAlive的有状态组件、已经被keepAlice的有状态组件 都是“有状态组件”，统一用 COMPONENT_STATEFUL 表示
+VNodeFlags.COMPONENT_STATEFUL =
+  VNodeFlags.COMPONENT_STATEFUL_NORMAL |
+  VNodeFlags.COMPONENT_STATEFUL_SHOULD_KEEP_ALIVE |
+  VNodeFlags.COMPONENT_STATEFUL_KEPT_ALIVE;
+// 有状态组件 和  函数式组件都是“组件”，用 COMPONENT 表示
+VNodeFlags.COMPONENT =
+  VNodeFlags.COMPONENT_STATEFUL | VNodeFlags.COMPONENT_FUNCTIONAL;
+
+const ChildrenFlags = {
+  // 未知的 children 类型
+  UNKNOWN_CHILDREN: 0,
+  // 没有 children
+  NO_CHILDREN: 1,
+  // children 是单个 VNode
+  SINGLE_VNODE: 1 << 1,
+
+  // children 是多个拥有 key 的 VNode
+  KEYED_VNODES: 1 << 2,
+  // children 是多个没有 key 的 VNode
+  NONE_KEYED_VNODES: 1 << 3,
+};
+
+ChildrenFlags.MULTIPLE_VNODES =
+  ChildrenFlags.KEYED_VNODES | ChildrenFlags.NONE_KEYED_VNODES;
+
+/* 
+export interface VNode {
+  // _isVNode 属性在上文中没有提到，它是一个始终为 true 的值，有了它，我们就可以判断一个对象是否是 VNode 对象
+  _isVNode: true
+  // el 属性在上文中也没有提到，当一个 VNode 被渲染为真实 DOM 之后，el 属性的值会引用该真实DOM
+  el: Element | null
+  flags: VNodeFlags
+  tag: string | FunctionalComponent | ComponentClass | null
+  data: VNodeData | null
+  children: VNodeChildren
+  childFlags: ChildrenFlags
+}
+*/
+const Fragment = Symbol();
+const Portal = Symbol();
+
+function h(tag, data = null, children = null) {
+  let flags = null;
+  if (typeof tag === "string") {
+    flags = tag === "svg" ? VNodeFlags.ELEMENT_SVG : VNodeFlags.ELEMENT_HTML;
+  } else if (tag === Fragment) {
+    flags = VNodeFlags.FRAGMENT;
+  } else if (tag === Portal) {
+    flags = VNodeFlags.PORTAL;
+    tag = data && data.target;
+  } else {
+    //兼容vue2的对象组件
+    if (tag !== null && typeof tag === "object") {
+      flags = tag.functional
+        ? VNodeFlags.COMPONENT_FUNCTIONAL // 函数式组件
+        : VNodeFlags.COMPONENT_STATEFUL_NORMAL; // 有状态组件
+    } else if (typeof tag === "function") {
+      flags =
+        tag.prototype && tag.prototype.render
+          ? VNodeFlags.COMPONENT_STATEFUL_NORMAL // 有状态组件
+          : VNodeFlags.COMPONENT_FUNCTIONAL; // 函数式组件
+    }
+  }
+
+  let childFlags = null;
+  if (Array.isArray(children)) {
+    const { length } = children;
+    if (length === 0) {
+      childFlags = ChildrenFlags.NO_CHILDREN;
+    } else if (length === 1) {
+      childFlags = ChildrenFlags.SINGLE_VNODE;
+      children = children[0];
+    } else {
+      childFlags = ChildrenFlags.KEYED_VNODES;
+      children = normalizeVNodes(children);
+    }
+  } else if (children == null) {
+    childFlags = ChildrenFlags.NO_CHILDREN;
+  } else if (children._isVNode) {
+    childFlags = ChildrenFlags.SINGLE_VNODE;
+  } else {
+    childFlags = ChildrenFlags.SINGLE_VNODE;
+    children = createTextVNode(children + "");
+  }
+
+  return {
+    _isVNode: true,
+    flags,
+    tag,
+    data,
+    children,
+    childFlags,
+    el: null,
+  };
+}
+
+function normalizeVNodes(children) {
+  const newChildren = [];
+  // 遍历 children
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i];
+    if (child.key == null) {
+      // 如果原来的 VNode 没有key，则使用竖线(|)与该VNode在数组中的索引拼接而成的字符串作为key
+      child.key = "|" + i;
+    }
+    newChildren.push(child);
+  }
+  // 返回新的children，此时 children 的类型就是 ChildrenFlags.KEYED_VNODES
+  return newChildren;
+}
+
+function createTextVNode(text) {
+  return {
+    _isVNode: true,
+    // flags 是 VNodeFlags.TEXT
+    flags: VNodeFlags.TEXT,
+    tag: null,
+    data: null,
+    // 纯文本类型的 VNode，其 children 属性存储的是与之相符的文本内容
+    children: text,
+    // 文本节点没有子节点
+    childFlags: ChildrenFlags.NO_CHILDREN,
+  };
+}
+
+function render(vnode, container) {
+  const prevVNode = container.vnode;
+  if (prevVNode == null) {
+    if (vnode) {
+      mount(vnode, container);
+      container.vnode = vnode;
+    }
+  } else {
+    if (vnode) {
+      patch(prevVNode, vnode, container);
+      container.vnode = vnode;
+    } else {
+      container.removeChild(prevVNode.el);
+      container.vnode = null;
+    }
+  }
+}
+
+function mount(vnode, container) {
+  const { flags } = vnode;
+  if (flags & VNodeFlags.ELEMENT) {
+    // 挂载普通标签
+    mountElement(vnode, container);
+  } else if (flags & VNodeFlags.COMPONENT) {
+    // 挂载组件
+    mountComponent(vnode, container);
+  } else if (flags & VNodeFlags.TEXT) {
+    // 挂载纯文本
+    mountText(vnode, container);
+  } else if (flags & VNodeFlags.FRAGMENT) {
+    // 挂载 Fragment
+    mountFragment(vnode, container);
+  } else if (flags & VNodeFlags.PORTAL) {
+    // 挂载 Portal
+    mountPortal(vnode, container);
+  }
+}
+
+function patch(prevVNode, nextVNode, container) {
+  // 分别拿到新旧 VNode 的类型，即 flags
+  const nextFlags = nextVNode.flags
+  const prevFlags = prevVNode.flags
+
+  // 检查新旧 VNode 的类型是否相同，如果类型不同，则直接调用 replaceVNode 函数替换 VNode
+  // 如果新旧 VNode 的类型相同，则根据不同的类型调用不同的比对函数
+  if (prevFlags !== nextFlags) {
+    replaceVNode(prevVNode, nextVNode, container)
+  } else if (nextFlags & VNodeFlags.ELEMENT) {
+    patchElement(prevVNode, nextVNode, container)
+  } else if (nextFlags & VNodeFlags.COMPONENT) {
+    patchComponent(prevVNode, nextVNode, container)
+  } else if (nextFlags & VNodeFlags.TEXT) {
+    patchText(prevVNode, nextVNode)
+  } else if (nextFlags & VNodeFlags.FRAGMENT) {
+    patchFragment(prevVNode, nextVNode, container)
+  } else if (nextFlags & VNodeFlags.PORTAL) {
+    patchPortal(prevVNode, nextVNode)
+  }
+}
+const domPropsRE = /\[A-Z]|^(?:value|checked|selected|muted)$/;
+function mountElement(vnode, container, isSVG) {
+  isSVG = isSVG || vnode.flags & VNodeFlags.ELEMENT_SVG;
+  const el = isSVG
+    ? document.createElementNS("http://www.w3.org/2000/svg", vnode.tag)
+    : document.createElement(vnode.tag);
+  vnode.el = el;
+  const data = vnode.data;
+  if (data) {
+    for (let key in data) {
+      switch (key) {
+        case "style":
+          for (let k in data.style) {
+            el.style[k] = data.style[k];
+          }
+          break;
+        case "class":
+          if (isSVG) {
+            el.setAttribute("class", data[key]);
+          } else {
+            el.className = data[key];
+          }
+          break;
+        default:
+          if (key[0] === "o" && key[1] === "n") {
+            // 事件
+            el.addEventListener(key.slice(2), data[key]);
+          } else if (domPropsRE.test(key)) {
+            // 当作 DOM Prop 处理
+            el[key] = data[key];
+          } else {
+            // 当作 Attr 处理
+            el.setAttribute(key, data[key]);
+          }
+          break;
+      }
+    }
+  }
+
+  const childFlags = vnode.childFlags;
+  const children = vnode.children;
+  if (childFlags !== ChildrenFlags.NO_CHILDREN) {
+    if (childFlags & ChildrenFlags.SINGLE_VNODE) {
+      mount(children, el, isSVG);
+    } else if (childFlags & ChildrenFlags.MULTIPLE_VNODES) {
+      for (let i = 0; i < children.length; i++) {
+        mount(children[i], el, isSVG);
+      }
+    }
+  }
+
+  container.appendChild(el);
+}
+
+function mountText(vnode, container) {
+  const el = document.createTextNode(vnode.children);
+  vnode.el = el;
+  container.appendChild(el);
+}
+
+function mountFragment(vnode, container, isSVG) {
+  // 拿到 children 和 childFlags
+  const { children, childFlags } = vnode;
+  switch (childFlags) {
+    case ChildrenFlags.SINGLE_VNODE:
+      // 如果是单个子节点，则直接调用 mount
+      mount(children, container, isSVG);
+      vnode.el = children.el
+      break;
+    case ChildrenFlags.NO_CHILDREN:
+      // 如果没有子节点，等价于挂载空片段，会创建一个空的文本节点占位
+      const placeholder = createTextVNode("");
+      mountText(placeholder, container);
+      vnode.el = placeholder.el
+      break;
+    default:
+      // 多个子节点，遍历挂载之
+      for (let i = 0; i < children.length; i++) {
+        mount(children[i], container, isSVG);
+      }
+      vnode.el = children[0].el
+  }
+}
+
+function mountPortal(vnode, container) {
+  const { tag, children, childFlags } = vnode
+  const target = typeof tag === 'string' ? document.querySelector(tag) : tag
+  if (childFlags & ChildrenFlags.SINGLE_VNODE) {
+    mount(children, target)
+  } else if (childFlags & ChildrenFlags.MULTIPLE_VNODES) {
+    for (let i = 0; i < children.length; i++) {
+      mount(children[i], target)
+    }
+  }
+
+  // 占位的空文本节点
+  const placeholder = createTextVNode('')
+  // 将该节点挂载到 container 中
+  mountText(placeholder, container, null)
+  // el 属性引用该节点
+  vnode.el = placeholder.el
+}
+
+function mountComponent(vnode, container, isSVG) {
+  if (vnode.flags & VNodeFlags.COMPONENT_STATEFUL) {
+    mountStatefulComponent(vnode, container, isSVG)
+  } else {
+    mountFunctionalComponent(vnode, container, isSVG)
+  }
+}
+
+function mountStatefulComponent(vnode, container, isSVG) {
+  // 创建组件实例
+  const instance = new vnode.tag()
+  // 渲染VNode
+  instance.$vnode = instance.render()
+  // 挂载
+  mount(instance.$vnode, container, isSVG)
+  // el 属性值 和 组件实例的 $el 属性都引用组件的根DOM元素
+  instance.$el = vnode.el = instance.$vnode.el
+}
+
+function mountFunctionalComponent(vnode, container, isSVG) {
+  // 获取 VNode
+  const $vnode = vnode.tag()
+  // 挂载
+  mount($vnode, container, isSVG)
+  // el 元素引用该组件的根元素
+  vnode.el = $vnode.el
+}
+
+function replaceVNode(prevVNode, nextVNode, container) {
+  container.removeChild(prevVNode.el)
+  mount(nextVNode, container)
+}
+
+
+function patchElement(prevVNode, nextVNode, container) {
+  // 如果新旧 VNode 描述的是不同的标签，则调用 replaceVNode 函数使用新的 VNode 替换旧的 VNode
+  if (prevVNode.tag !== nextVNode.tag) {
+    replaceVNode(prevVNode, nextVNode, container)
+    return
+  }
+
+  // 拿到 el 元素，注意这时要让 nextVNode.el 也引用该元素
+  const el = (nextVNode.el = prevVNode.el)
+  const prevData = prevVNode.data
+  const nextData = nextVNode.data
+
+  if (nextData) {
+    for (let key in nextData) {
+      const prevValue = prevData[key]
+      const nextValue = nextData[key]
+      patchData(el, key, prevValue, nextValue)
+    }
+  }
+  if (prevData) {
+    for (let key in prevData) {
+      const prevValue = prevData[key]
+      if (prevValue && !nextData.hasOwnProperty(key)) {
+        patchData(el, key, prevValue, null)
+      }
+    }
+  }
+
+  patchChildren(
+    prevVNode.childFlags, // 旧的 VNode 子节点的类型
+    nextVNode.childFlags, // 新的 VNode 子节点的类型
+    prevVNode.children,   // 旧的 VNode 子节点
+    nextVNode.children,   // 新的 VNode 子节点
+    el                    // 当前标签元素，即这些子节点的父节点
+  )
+}
+
+function patchData(el, key, prevValue, nextValue) {
+  switch (key) {
+    case 'style':
+      for (let k in nextValue) {
+        el.style[k] = nextValue[k]
+      }
+      for (let k in prevValue) {
+        if (!nextValue.hasOwnProperty(k)) {
+          el.style[k] = ''
+        }
+      }
+      break
+    case 'class':
+      el.className = nextValue
+      break
+    default:
+      if (key[0] === 'o' && key[1] === 'n') {
+        // 事件
+        // 移除旧事件
+        if (prevValue) {
+          el.removeEventListener(key.slice(2), prevValue)
+        }
+        // 添加新事件
+        if (nextValue) {
+          el.addEventListener(key.slice(2), nextValue)
+        }
+      } else if (domPropsRE.test(key)) {
+        // 当做 DOM Prop 处理
+        el[key] = nextValue
+      } else {
+        // 当做 Attr 处理
+        el.setAttribute(key, nextValue)
+      }
+      break
+  }
+}
+
+function patchChildren(
+  prevChildFlags,
+  nextChildFlags,
+  prevChildren,
+  nextChildren,
+  container
+) {
+  switch (prevChildFlags) {
+    // 旧的 children 是单个子节点，会执行该 case 语句块
+    case ChildrenFlags.SINGLE_VNODE:
+      switch (nextChildFlags) {
+        case ChildrenFlags.SINGLE_VNODE:
+          // 新的 children 也是单个子节点时，会执行该 case 语句块
+          patch(prevChildren, nextChildren, container)
+          break
+        case ChildrenFlags.NO_CHILDREN:
+          // 新的 children 中没有子节点时，会执行该 case 语句块
+          container.removeChild(prevChildren.el)
+          break
+        default:
+          // 但新的 children 中有多个子节点时，会执行该 case 语句块
+          container.removeChild(prevChildren.el)
+          for (let i = 0; i < nextChildren.length; i++) {
+            mount(nextChildren[i], container)
+          }
+          break
+      }
+      break
+    // 旧的 children 中没有子节点时，会执行该 case 语句块
+    case ChildrenFlags.NO_CHILDREN:
+      switch (nextChildFlags) {
+        case ChildrenFlags.SINGLE_VNODE:
+          // 新的 children 是单个子节点时，会执行该 case 语句块
+          mount(nextChildren, container)
+          break
+        case ChildrenFlags.NO_CHILDREN:
+          // 新的 children 中没有子节点时，会执行该 case 语句块
+          break
+        default:
+          // 但新的 children 中有多个子节点时，会执行该 case 语句块
+          for (let i = 0; i < nextChildren.length; i++) {
+            mount(nextChildren[i], container)
+          }
+          break
+      }
+      break
+    // 旧的 children 中有多个子节点时，会执行该 case 语句块
+    default:
+      switch (nextChildFlags) {
+        case ChildrenFlags.SINGLE_VNODE:
+          for (let i = 0; i < prevChildren.length; i++) {
+            container.removeChild(prevChildren[i].el)
+          }
+          mount(nextChildren, container)
+          break
+        case ChildrenFlags.NO_CHILDREN:
+          for (let i = 0; i < prevChildren.length; i++) {
+            container.removeChild(prevChildren[i].el)
+          }
+          break
+        default:
+          // 遍历旧的子节点，将其全部移除
+          for (let i = 0; i < prevChildren.length; i++) {
+            container.removeChild(prevChildren[i].el)
+          }
+          // 遍历新的子节点，将其全部添加
+          for (let i = 0; i < nextChildren.length; i++) {
+            mount(nextChildren[i], container)
+          }
+          break
+      }
+      break
+  }
+}
+
+function patchText(prevVNode, nextVNode) {
+  // 拿到文本元素 el，同时让 nextVNode.el 指向该文本元素
+  const el = (nextVNode.el = prevVNode.el)
+  // 只有当新旧文本内容不一致时才有必要更新
+  if (nextVNode.children !== prevVNode.children) {
+    el.nodeValue = nextVNode.children
+  }
+}
+
+function patchFragment(prevVNode, nextVNode, container) {
+  // 直接调用 patchChildren 函数更新 新旧片段的子节点即可
+  patchChildren(
+    prevVNode.childFlags, // 旧片段的子节点类型
+    nextVNode.childFlags, // 新片段的子节点类型
+    prevVNode.children,   // 旧片段的子节点
+    nextVNode.children,   // 新片段的子节点
+    container
+  )
+
+  switch (nextVNode.childFlags) {
+    case ChildrenFlags.SINGLE_VNODE:
+      nextVNode.el = nextVNode.children.el
+      break
+    case ChildrenFlags.NO_CHILDREN:
+      nextVNode.el = prevVNode.el
+      break
+    default:
+      nextVNode.el = nextVNode.children[0].el
+  }
+}
+
+function patchPortal(prevVNode, nextVNode) {
+  patchChildren(
+    prevVNode.childFlags,
+    nextVNode.childFlags,
+    prevVNode.children,
+    nextVNode.children,
+    prevVNode.tag // 注意 container 是旧的 container
+  )
+  // 让 nextVNode.el 指向 prevVNode.el
+  nextVNode.el = prevVNode.el
+
+  // 如果新旧容器不同，才需要搬运
+  if (nextVNode.tag !== prevVNode.tag) {
+    // 获取新的容器元素，即挂载目标
+    const container =
+      typeof nextVNode.tag === 'string'
+        ? document.querySelector(nextVNode.tag)
+        : nextVNode.tag
+
+    switch (nextVNode.childFlags) {
+      case ChildrenFlags.SINGLE_VNODE:
+        // 如果新的 Portal 是单个子节点，就把该节点搬运到新容器中
+        container.appendChild(nextVNode.children.el)
+        break
+      case ChildrenFlags.NO_CHILDREN:
+        // 新的 Portal 没有子节点，不需要搬运
+        break
+      default:
+        // 如果新的 Portal 是多个子节点，遍历逐个将它们搬运到新容器中
+        for (let i = 0; i < nextVNode.children.length; i++) {
+          container.appendChild(nextVNode.children[i].el)
+        }
+        break
+    }
+  }
+}
