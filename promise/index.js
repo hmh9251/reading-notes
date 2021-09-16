001c12baa8b9bc6a37a54e63adeb476c0e2cc621
kexi <hmh9925991@gmail.com>
2021-09-16 12:01:53 +0800

    实现一个promise

--- promise/index.js
+++ promise/index.js
@@ -1,119 +1,94 @@
 /*
- * @Desc: 
+ * @Desc: 手写一个promise
  * @Author: kexi
  * @Date: 2020-07-17 17:09:52
  * @LastEditors: kexi
- * @LastEditTime: 2021-05-20 11:45:38
+ * @LastEditTime: 2021-09-16 12:01:12
  */ 
 
- const PENDING = "PENDING";
- const FULLFILLED = "FULLFILLED";
- const REJECTED = "REJECTED";
+const PENDING = "PENDING";
+const FULLFILLED = "FULLFILLED";
+const REJECTED = "REJECTED";
 
-class Promise {
-  constructor(executor) {
+class Promise{
+  constructor(fn) {
     this.state = PENDING;
     this.value = undefined;
-    this.reason = undefined;
-
-    // 成功存放的数组
-    this.onResolvedCallbacks = [];
-    // 失败存放法数组
-    this.onRejectedCallbacks = [];
-
-    let resolve = value => {
-      if (this.state === PENDING) {
-        this.state = FULLFILLED;
-        this.value = value;
-        this.onResolvedCallbacks.forEach(fn => fn());
-      }
+    this.error = undefined;
+    this.onFullCallback = [];
+    this.onFailCallback = [];
+    // 这里要用订阅发布模式，等到方法执行完毕，才去调用then里面的方法
+    const resolve = (value) => {
+      this.value = value;
+      this.state = FULLFILLED;
+      // 循环所有方法并执行
+      this.onFullCallback.forEach(cb => {
+        cb(this.value);
+      })
     }
 
-    let reject = reason => {
-      if (this.state = PENDING) {
-        this.state = REJECTED;
-        this.reason = reason;
-        this.onRejectedCallbacks.forEach(fn => fn());
-      }
+    const reject = (value) => {
+      this.error = value;
+      this.state = REJECTED;
+      this.onFailCallback.forEach(cb => {
+        cb(this.error);
+      })
     }
-
-    try {
-      executor(resolve, reject);
-    } catch(err) {
-      console.log('43');
-      reject(err);
-    }
+    
+    fn(resolve, reject);
   }
 
-  then(onFullfilled, onRejected) {
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
-
-    let promise2 = new Promise((resolve, reject) => {
-      if (this.state === FULLFILLED) {
-        let x = onFullfilled(this.value);
+  then(cb1, cb2) {
+    let state = this.state;
+    var promise2 = new Promise((resolve, reject) => {
+      if (state === FULLFILLED) {
+        let x = cb1(this.value);
         resolvePromise(promise2, x, resolve, reject);
-      }
-      if (this.state === REJECTED) {
-        let x = onRejected(this.reason);
+      } else if (state === REJECTED) {
+        let x = cb2(this.error);
         resolvePromise(promise2, x, resolve, reject);
-      }
-      // 当状态state为pending时
-      if (this.state === PENDING) {
-        // onFulfilled传入到成功数组
-        this.onResolvedCallbacks.push(()=>{
-          let x = onFullfilled(this.value);
+      } else {
+        this.onFullCallback.push(() => {
+          let x = cb1(this.value);
           resolvePromise(promise2, x, resolve, reject);
-        })
-        // onRejected传入到失败数组
-        this.onRejectedCallbacks.push(()=>{
-          let x = onRejected(this.reason);
+        });
+        this.onFailCallback.push(() => {
+          let x = cb2(this.error);
           resolvePromise(promise2, x, resolve, reject);
-        })
+        });
       }
     })
-
-    return promise2;
+    return promise;
   }
 }
 
-function resolvePromise(promise2, x, resolve, reject){
-  // 循环引用报错
-  if(x === promise2){
-    // reject报错
-    return reject(new TypeError('Chaining cycle detected for promise'));
+function resolvePromise(promise, x, resolve, reject)  {
+  if (promise === x) {
+    return reject(new TypeError('Chaining cycle detected for promise'))
   }
-  // 防止多次调用
   let called;
-  // x不是null 且x是对象或者函数
   if (x != null && (typeof x === 'object' || typeof x === 'function')) {
     try {
-      // A+规定，声明then = x的then方法
       let then = x.then;
-      // 如果then是函数，就默认是promise了
-      if (typeof then === 'function') { 
-        // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
+      if (typeof then === 'function') {
         then.call(x, y => {
-          // 成功和失败只能调用一个
           if (called) return;
           called = true;
-          // resolve的结果依旧是promise 那就继续解析
-          resolvePromise(promise2, y, resolve, reject);
+          resolvePromise(promise, y, resolve, reject);
         }, err => {
-          // 成功和失败只能调用一个
           if (called) return;
           called = true;
-          reject(err);// 失败了就失败了
+          reject(err);
         })
       } else {
-        resolve(x); // 直接成功即可
+        if (called) return;
+        called = true;
+        resolve(x);
       }
-    } catch (e) {
-      // 也属于失败
+    } catch (error) {
       if (called) return;
       called = true;
-      // 取then出错了那就不要在继续执行了
-      reject(e); 
+      reject(error);
     }
   } else {
     resolve(x);
@@ -121,27 +96,31 @@ function resolvePromise(promise2, x, resolve, reject){
 }
 
 
-let p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('就这样')
-  }, 2000)
+// demo1
+const promise = new Promise(function(resolve, reject) {
+  // ... some code
+  if (1){
+    setTimeout(() => {
+      resolve('success');
+    }, 1000);
+  } else {
+    reject('fail');
+  }
 });
-p.then(data => {
-  console.log('72')
-  console.log(data)
-  return '那不这样呢';
-}, err => {
-  console.log('74')
-  console.log(err)
-}).then(data => {
-  console.log('133');
+
+promise.then((data) => {
+  console.log('cb1');
   console.log(data);
+}, err => {
+  console.log(err);
+}).then(data => {
+  console.log('cb1: then2')
+  console.log(data);
+}, err => {
+  console.log(err);
 })
 
-/* p.then(data => {
-  console.log('78')
-  console.log(data)
-}, err => {
-  console.log('82')
-  console.log(err)
-}); */
+/* promise.then(data => {
+  console.log('cb2');
+  console.log(data);
+}) */
